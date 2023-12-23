ea7d3e4eac9ce43ab530b13cdc3ddcd6fa4a14e0
Caprice Yuri <123914679+CapriceYuri@users.noreply.github.com>
2023-12-23 12:33:39 -0500

    Swap anchor to Link Tag

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -15,7 +15,7 @@ function Navigation() {
           <ul className="flex w-full md:w-3/4 lg:w-1/2 mx-auto justify-center gap-x-10 p-5 font-medium text-md text-white">
             {linkObj.map((item) => (
               <li key={item.id}>
-                <a href={item.href}>{item.linkName}</a>
+                <Link to={item.href}>{item.linkName}</Link>
               </li>
             ))}
           </ul>
